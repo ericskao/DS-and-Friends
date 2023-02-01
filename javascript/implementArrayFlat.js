b9b5c321087e35ca7292e6e9e80beab45937c4e3
Eric K <ericskao@gmail.com>
2023-02-01 14:01:50 -0800

    array flat

---
+++ javascript/implementArrayFlat.js
@@ -0,0 +1,50 @@
+// 3. implement Array.prototype.flat()
+
+// There is already Array.prototype.flat() in JavaScript (ES2019), which reduces the nesting of Array.
+
+// Could you manage to implement your own one?
+
+// Here is an example to illustrate
+
+// const arr = [1, [2], [3, [4]]];
+
+// flat(arr)
+// // [1, 2, 3, [4]]
+
+// flat(arr, 1)
+// // [1, 2, 3, [4]]
+
+// flat(arr, 2)
+// // [1, 2, 3, 4]
+
+// This is a JavaScript coding problem from BFE.dev
+/**
+ * @param { Array } arr
+ * @param { number } depth
+ * @returns { Array }
+ */
+
+function flat(arr, depth = 1) {
+  // flatten any arrays one level deep
+  let result = []
+  for(let i = 0; i<arr.length; i++){
+    if(Array.isArray(arr[i]) && depth > 0){
+      result.push(...flat(arr[i], depth-1))
+    } else {
+      result.push(arr[i])
+    }
+  }
+  return result
+}
+
+const arr = [1, [2], [3, [4]]];
+
+console.log(flat(arr))
+// [1, 2, 3, [4]]
+
+console.log(flat(arr, 1))
+// [1, 2, 3, [4]]
+
+// flat(arr, 2)
+// [1, 2, 3, 4]
+console.log('hi')
