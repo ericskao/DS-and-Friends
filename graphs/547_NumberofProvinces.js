f7d40eba952e291fbb9627229f9d0e7c0974dccc
Eric K <ericskao@gmail.com>
2023-02-04 13:46:40 -0800

    start graphs

---
+++ graphs/547_NumberofProvinces.js
@@ -0,0 +1,54 @@
+// 547. Number of Provinces
+
+// There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+
+// A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+// You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+
+// Return the total number of provinces.
+
+// Example 1:
+// Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+// Output: 2
+
+// Example 2:
+// Input: isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+// Output: 3
+
+// Constraints:
+// 1 <= n <= 200
+// n == isConnected.length
+// n == isConnected[i].length
+// isConnected[i][j] is 1 or 0.
+// isConnected[i][i] == 1
+// isConnected[i][j] == isConnected[j][i]
+
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+
+// use DFS where we follow along the node and change each to visited
+var findCircleNum = function(isConnected) {
+    let n = isConnected.length
+    let visited = Array(n).fill(0)
+    let provinces = 0
+    const dfs = (cityIndex) => {
+        visited[cityIndex] = 1
+        let cityRow = isConnected[cityIndex]
+        for(let j = 0; j<cityRow.length; j++){
+            if(cityRow[j] === 1 && j !== cityIndex && !visited[j]){
+                dfs(j)
+            }
+        }
+    }
+    for(let i = 0; i<n; i++){
+        if(!visited[i]){ // if not visited
+            // dfs and mark all connected cities as visited
+            provinces++
+            dfs(i)
+        }
+    }
+    return provinces
+};
