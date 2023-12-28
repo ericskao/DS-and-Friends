1f0ad1eaa14907d8550dcf3d8a66118035f7ddbe
Eric K <ericskao@gmail.com>
2023-12-28 14:15:15 -0800

    mergeAlternative brute

---
+++ leetCode-top-75/mergeStringsAlternatively.js
@@ -0,0 +1,56 @@
+// 1768. Merge Strings Alternately
+
+// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+// Return the merged string.
+
+// Example 1:
+// Input: word1 = "abc", word2 = "pqr"
+// Output: "apbqcr"
+// Explanation: The merged string will be merged as so:
+// word1:  a   b   c
+// word2:    p   q   r
+// merged: a p b q c r
+
+// Example 2:
+// Input: word1 = "ab", word2 = "pqrs"
+// Output: "apbqrs"
+// Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+// word1:  a   b
+// word2:    p   q   r   s
+// merged: a p b q   r   s
+
+// Example 3:
+// Input: word1 = "abcd", word2 = "pq"
+// Output: "apbqcd"
+// Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+// word1:  a   b   c   d
+// word2:    p   q
+// merged: a p b q c   d
+
+// Constraints:
+// 1 <= word1.length, word2.length <= 100
+// word1 and word2 consist of lowercase English letters.
+
+
+// Brute force
+const mergeAlternately = function(str1, str2){
+  let output = ''
+  let iterations = str1.length >= str2.length ? str1.length : str2.length
+  let arr1 = str1.split('')
+  let arr2 = str2.split('')
+
+  while(iterations > 0){
+    if(arr1[0]){
+      output += arr1.shift()
+    }
+    if(arr2[0]){
+      output += arr2.shift()
+    }
+    iterations--
+  }
+
+  return output
+}
+
+// Two pointer
